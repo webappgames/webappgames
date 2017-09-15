e6fa40dda190d8d89284b15f0aade44b85bcae24
hejny <me@pavolhejny.com>
2017-09-15 02:31:56 +0200

    + OIMO

--- src/scene/create-scene.ts
+++ src/scene/create-scene.ts
@@ -17,9 +17,11 @@ function getMaterial(name:string,scene:BABYLON.Scene){
 
 export default function createScene(canvas: HTMLCanvasElement, engine: BABYLON.Engine): BABYLON.Scene {
     const scene = new BABYLON.Scene(engine);
+
+
     scene.clearColor = new BABYLON.Color4(1, 1, 1, 0);
 
-    const camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(-10, 2, -10), scene);
+    const camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(-50, 6, -70), scene);
     camera.fov = 1.2;
 
     camera.attachControl(canvas, true);
@@ -39,6 +41,15 @@ export default function createScene(canvas: HTMLCanvasElement, engine: BABYLON.E
     camera.ellipsoid = new BABYLON.Vector3(2, 3, 2);
 
 
+    const gravityVector = new BABYLON.Vector3(0,-9.81, 0);
+    //const physicsPlugin = new BABYLON.CannonJSPlugin();
+    const physicsPlugin = new BABYLON.OimoJSPlugin()
+    scene.enablePhysics(gravityVector, physicsPlugin);
+    //scene.enablePhysics();
+
+
+
+
     const light1 = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(-1, -2, -1), scene);
     light1.position = new BABYLON.Vector3(20, 3, 20);
     light1.intensity = 1;
@@ -53,6 +64,11 @@ export default function createScene(canvas: HTMLCanvasElement, engine: BABYLON.E
     groundMesh.position.y = -0.5;
     groundMesh.checkCollisions = true;
     groundMesh.material = getMaterial('grass',scene);
+    groundMesh.physicsImpostor = new BABYLON.PhysicsImpostor(groundMesh, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.1 }, scene);
+
+
+
+
 
     const stairsMesh = createStairs(scene, 50);
     stairsMesh.position.y = -0.5;
@@ -68,16 +84,19 @@ export default function createScene(canvas: HTMLCanvasElement, engine: BABYLON.E
 
 
     for(let i=0;i<100;i++){
-        const box = BABYLON.Mesh.CreateBox("box", 4, scene);
-        box.position.y = 0;
-        box.position.x = (Math.random()-0.5)*100;
-        box.position.z = (Math.random()-0.5)*100;
-        box.rotation.y = Math.random()*Math.PI*2;
+        const boxMesh = BABYLON.Mesh.CreateBox("box", 4, scene);
+        boxMesh.position.y = Math.random()*20;
+        boxMesh.position.x = (Math.random()-0.5)*100;
+        boxMesh.position.z = (Math.random()-0.5)*100;
+        boxMesh.rotation.y = Math.random()*Math.PI*2;
 
-        box.rotation.x = Math.random()*Math.PI*2/20;
-        box.rotation.z = Math.random()*Math.PI*2/20;
-        box.checkCollisions = true;
-        box.material = getMaterial('stone-plain',scene);
+        boxMesh.rotation.x = Math.random()*Math.PI*2/20;
+        boxMesh.rotation.z = Math.random()*Math.PI*2/20;
+        boxMesh.checkCollisions = true;
+        boxMesh.material = getMaterial('stone-plain',scene);
+
+
+        boxMesh.physicsImpostor = new BABYLON.PhysicsImpostor(boxMesh, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, restitution: 0.9 }, scene);
     }
 
 
@@ -89,10 +108,63 @@ export default function createScene(canvas: HTMLCanvasElement, engine: BABYLON.E
 
 
 
-    scene.registerBeforeRender(()=>{
-        camera.cameraDirection.y += 0.01;
-    });
+    //scene.registerBeforeRender(()=>{camera.cameraDirection.y += 0.01;});
+
 
 
     return scene;
 }
+
+
+
+
+/*
+
+export default function createScene(canvas: HTMLCanvasElement, engine: BABYLON.Engine): BABYLON.Scene {
+
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new BABYLON.Scene(engine);
+
+    // This creates and positions a free camera (non-mesh)
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 50, -100), scene);
+
+    // This targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    scene.enablePhysics(new BABYLON.Vector3(0,-20, 0), new BABYLON.OimoJSPlugin());
+
+
+    for(let i=0;i<100;i++){
+        var box = BABYLON.Mesh.CreateBox("sphere1", 2, scene);
+        box.position.y =Math.random()*100;
+        box.position.x = (Math.random()-0.5)*20;
+        box.position.z = (Math.random()-0.5)*20;
+
+        box.rotation.x = Math.random()*Math.PI*2;
+        box.rotation.y = Math.random()*Math.PI*2;
+        box.rotation.z = Math.random()*Math.PI*2;
+
+        box.physicsImpostor = new BABYLON.PhysicsImpostor(box, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 10, restitution: 0.5 }, scene);
+    }
+
+
+    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+    var ground = BABYLON.Mesh.CreateGround("ground1", 1000, 1000, 2, scene);
+
+
+
+
+    ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+
+    return scene;
+
+};/**/
