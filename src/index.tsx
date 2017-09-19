7471b0a70533477429f49e407e2e00593f72ac77
hejny <me@pavolhejny.com>
2017-09-19 21:33:36 +0200

    + ui cross

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import './index.css';
 import * as BABYLON from 'babylonjs';
 import createScene from './scene/create-scene';
-import registerServiceWorker from './registerServiceWorker';
-registerServiceWorker();
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Root from './ui/components/Root';
+//import registerServiceWorker from './registerServiceWorker';
+//registerServiceWorker();
 
 const canvasElement = document.getElementById("scene") as any;
-
-
+const uiElement = document.getElementById("ui") as any;
 
 
 
@@ -20,3 +22,11 @@ engine.runRenderLoop(function () {
 window.addEventListener("resize", function () {
     engine.resize();
 });
+
+
+
+ReactDOM.render(
+    <Root />,
+    uiElement
+);
+
---
+++ src/ui/components/Root.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './style/Root.css';
+
+export default function Root(props:{}){
+    return(
+        <div>
+            <img id="cross" src="/assets/ui/cross.png"/>
+        </div>
+    );
+}
