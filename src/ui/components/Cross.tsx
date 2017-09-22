07da62929398d79029903b85f669d2959e7039cf
hejny <me@pavolhejny.com>
2017-09-23 01:42:44 +0200

    + Spells - formating numbers

--- src/ui/components/Cross.tsx
+++ src/ui/components/Cross.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {observer} from 'mobx-react';
 import DataModel from '../../data-model';
+import {formatNumber} from '../../tools/number';
 import './style/Cross.css';
 
 
@@ -9,6 +10,8 @@ export default observer(({dataModel}:{dataModel:DataModel})=> {
     return (
         <div id="cross" >
             <img src={`/assets/ui/cross${dataModel.aimed?'-aimed':''}.png`}/>
+            {formatNumber(1000,'ε')}
+
         </div>
     );
 });
