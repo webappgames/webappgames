f4734e0df2b7a5c24cc0d24084ff522dd3b272af
hejny <me@pavolhejny.com>
2017-09-23 14:36:09 +0200

    + Spells / Teleport

--- src/spells/game-spells/01-subjective/Teleport.ts
+++ src/spells/game-spells/01-subjective/Teleport.ts
@@ -10,7 +10,7 @@ export default class Teleport extends Spell{
         //todo better with clone
         //todo teleport to target point not to middle of target mesh
         this.playerMesh.position.x = this.targetMesh.position.x;
-        this.playerMesh.position.y = this.targetMesh.position.y;
+        this.playerMesh.position.y = this.targetMesh.position.y+this.targetMesh.scaling.y/2+2;
         this.playerMesh.position.z = this.targetMesh.position.z;
         //this.playerMesh.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0,100,0));
     }
