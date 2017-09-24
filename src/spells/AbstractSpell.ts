a13e2346b44dc4ee7052742b574c92dd84749052
hejny <me@pavolhejny.com>
2017-09-25 01:39:38 +0200

    + Spells- AbstractSpell

--- src/spells/AbstractSpell.ts
+++ src/spells/AbstractSpell.ts
@@ -1,78 +1,72 @@
 import * as BABYLON from 'babylonjs';
 
+export enum spellPhases{
+    PREPARING,
+    PREPARED,
+    EXECUTING,
+    FINISHED
+}
+
 export default class AbstractSpell{
-    /*
-    TARGETS = 1
-    -----------
-
-
-    TARGETS > 1
-    -----------
-
-    TARGETS = 0
-    -----------
-    Spell that interacts with user or whole environment.
-
-    Imediate, Durable//todo
-    */
-    public TARGETS = 1;
-
-
-
-
-    //todo only with targets
-    public direction = BABYLON.Vector3.Zero();
-
-
-    acceptTargetMesh(){
-            return this.targetMesh.name!=='ground'
-    }
-
-    get dynamicTarget():BABYLON.Vector3{
-        return this.targetMesh.position;
-    }
-    get dynamicSpeed():number{
-        return 100;
-    }
-
-
-
-
-
-
-
-
 
     constructor(
-        public targetMeshes:BABYLON.AbstractMesh[],
-        public targetPoints:BABYLON.Vector3[],
+        //public targetMeshes:BABYLON.AbstractMesh[],
+        //public targetPoints:BABYLON.Vector3[],
+        public costCallback:(cost:number)=>boolean,
+        public gainCallback:(cost:number)=>boolean,
+        public otherPlayerSpell:AbstractSpell[],
         public playerMesh:BABYLON.AbstractMesh,
         public scene:BABYLON.Scene,
     ){
+        this.phase = spellPhases.PREPARING;
     }
 
-
-
-
-    //todo in future thare should be more resources than energy
-    countEnergyCost():number{
-        return 0;
+    private _phase:spellPhases;
+    private _phaseSubscribers:(()=>void)[];
+    set phase(phase:spellPhases){
+        this._phase = phase;
+        this._phaseSubscribers.forEach((subscriberCallback:()=>void)=>{
+            subscriberCallback();
+        });
     }
-    countEnergyGain():number{
-        return 0;
+    get phase():spellPhases{
+        return this._phase;
+    }
+    subscribe(subscriberCallback:()=>void){
+        this._phaseSubscribers.push(subscriberCallback);
     }
 
-
-
-
-
-    begin(){
+    public targets:BABYLON.PickingInfo[];
+    addTarget(target:BABYLON.PickingInfo){
+        this.targets.push(target);
+        if(this.isPrepared){
+            this.phase = spellPhases.PREPARED;
+        }
     }
+
+    //this is overwritten by extended classes
+    public isPrepared=false;
+    public message = '';
+
+
     execute(){
+        if(this.phase === spellPhases.PREPARED){
+            this.phase = spellPhases.EXECUTING;
+
+            //todo new SpellEffect
+        }
     }
 
+    tick(){
+        if(this.phase === spellPhases.EXECUTING) {
+            //todo new SpellEffect...
+        }
+    }
 
-
-
-
+    finish(){
+        if(this.phase === spellPhases.EXECUTING) {
+            this.phase = spellPhases.FINISHED;
+            //todo new SpellEffect...
+        }
+    }
 }
