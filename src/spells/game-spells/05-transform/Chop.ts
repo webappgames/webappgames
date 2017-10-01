1f427c72be575fcaa7f06eb6c4dbf176e7c8e757
hejny <me@pavolhejny.com>
2017-10-01 23:02:36 +0200

    + Chop

--- src/spells/game-spells/05-transform/Chop.ts
+++ src/spells/game-spells/05-transform/Chop.ts
@@ -2,11 +2,17 @@ import AbstractSplit from './AbstractSplit';
 
 export default class Chop extends AbstractSplit{
     get splitParts():{x:number,y:number,z:number}{
-        //todo
+        const dimensions = ['x','y','z'].map((axis)=>({
+                axis,
+                scaling: this.firstTargetMesh.scaling[axis]
+            })).sort((a,b)=>a.scaling<b.scaling?1:-1);
+        //console.log(dimensions);
+        const axis = dimensions[0].axis;
+
         return {
-            x:1,
-            y:2,
-            z:1
+            x:axis==='x'?2:1,
+            y:axis==='y'?2:1,
+            z:axis==='z'?2:1
         }
     }
 }
